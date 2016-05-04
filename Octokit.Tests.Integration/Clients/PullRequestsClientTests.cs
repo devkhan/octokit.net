3edae1a9145419f4d95562a5c0a3a55d0d62d2ce
Sarmad <SarmadSattar1@gmail.com>
2016-05-04 20:35:57 +0500

    Some Changes

--- Octokit.Tests.Integration/Clients/PullRequestsClientTests.cs
+++ Octokit.Tests.Integration/Clients/PullRequestsClientTests.cs
@@ -230,14 +230,15 @@ public class PullRequestsClientTests : IDisposable
     {
         await CreateTheWorld();
 
-        var newPullRequest = new NewPullRequest("a pull request", branchName, "master");
+        var newPullRequest = new NewPullRequest("squash commit pull request", branchName, "master");
         var pullRequest = await _fixture.Create(Helper.UserName, _context.RepositoryName, newPullRequest);
 
-        var merge = new MergePullRequest { CommitMessage = "fake message", CommitTitle = "fake title", Squash = true };
+        var merge = new MergePullRequest { CommitMessage = "fake commit message", CommitTitle = "fake title", Squash = true };
         var result = await _fixture.Merge(Helper.UserName, _context.RepositoryName, pullRequest.Number, merge);
         var commit = await _github.Repository.Commit.Get(_context.RepositoryOwner, _context.RepositoryName, result.Sha);
 
-        Assert.Equal(merge.CommitMessage, commit.Commit.Message);
+        Assert.Equal("fake commit message", commit.Commit.Message);
+        Assert.True(result.Merged);
     }
 
     [IntegrationTest]
